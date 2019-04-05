5cc3469f352bc4742c6fc2e089f6300c333cb29b
Tsimwi <33039189+Tsimwi@users.noreply.github.com>
2019-04-05 11:32:36 +0200

    fix pom.xml

---
+++ src/test/java/PieceTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PieceTest {
+
+    @Test
+    void testPieceNameAndLocation() {
+        Square jail = new Square("Jail");
+        Piece red = new Piece("Red", jail);
+
+        assertEquals(red.getName(), "Red");
+        assertEquals(red.getLocation(), "Jail");
+
+        Square bank = new Square("Bank");
+        red.setName("Pink");
+        red.setLocation(bank);
+
+        assertEquals(red.getName(), "Pink");
+        assertEquals(red.getLocation(), bank);
+    }
+}
